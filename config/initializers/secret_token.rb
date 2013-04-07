5d5d2869e3459390222c95fab3dab1d66f09d52c
Jack Cameo <jack@klatchit.com>
2013-04-07 16:58:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'e6615f8da8b37529d70d4459e5f613cc223d207add1e78875bd12e444721290a08661a6bc8fe0461bdc772caeea09178124472b1455835567c5be3045da7ead6'
---
+++ vendor/bundle/gems/pg-0.12.2/ext/siteconf20130407-37349-1vtn1dp.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130407-37349-7raa1p"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
